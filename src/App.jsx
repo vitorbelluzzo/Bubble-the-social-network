182765b23a2d4255e407c8a9e822e19c45e839e9
vitorbelluzzo <vitorbelluzzo@hotmail.com>
2024-01-19 23:31:07 -0300

    Estrutura da aplicação - CSS Modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { Header } from './components/Header';
 import { Post } from './post';
+import './styles.css';
 
 export function App() {
   return (
     <div>
+
+      <Header />
+          
       <Post
         author="Vitor "
         content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta recusandae autem esse, architecto ex deleniti illo, ea in cum adipisci sequi a, ipsum fuga. Obcaecati vel eum et eaque quisquam."
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,3 @@
-// teste
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
