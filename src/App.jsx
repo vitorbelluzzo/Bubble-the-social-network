41912a58b341b85c108681955d4bf3739fecd4b2
vitorbelluzzo <vitorbelluzzo@hotmail.com>
2024-01-21 02:22:44 -0300

    Estrutura da aplicação - Componente: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
 import { Header } from './components/Header';
 import { Post } from './post';
 import './global.css';
+import styles from './app.module.css';
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
     <div>
-
       <Header />
-          
-      <Post
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+        <Post
         author="Vitor "
         content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta recusandae autem esse, architecto ex deleniti illo, ea in cum adipisci sequi a, ipsum fuga. Obcaecati vel eum et eaque quisquam."
-      />
-      <Post
+        />
+        <Post
         author="Sophia "
         content="Post incrivel!"
-      />
+        />
+        </main>
+      </div> 
+      
     </div>
 
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,12 @@
-import styles from './Header.module.css'
-
+import styles from './Header.module.css';
 import bubblelogo from '../assets/bubble-logo.svg';
 import bubbletext from '../assets/bubble-text.svg';
-
-console.log(bubblelogo)
 
 export function Header() {
     return (
         <header className={styles.header}>
             <img src={bubblelogo} alt="logotipo bubble" />
             <img src={bubbletext} alt="logotipo bubble" />
-            
         </header>
-            
-        );
+    );
 }
