02604f61754af641e29585f71fb7566c203f5433
vitorbelluzzo <vitorbelluzzo@hotmail.com>
2024-01-22 23:20:00 -0300

    Estrutura da aplicação - Estilizações do Post

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,9 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-        <Post
-        author="Vitor "
-        content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta recusandae autem esse, architecto ex deleniti illo, ea in cum adipisci sequi a, ipsum fuga. Obcaecati vel eum et eaque quisquam."
-        />
+        <Post/>
+        <Post/>
+        <Post/>
        
         </main>
       </div> 
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,7 +2,7 @@ import styles from './Post.module.css';
 export function Post() {
   return (
     <article className={styles.post}>
-      <header className='header'>
+      <header>
         <div className={styles.author}>
           {<img className={styles.avatar} src="https://avatars.githubusercontent.com/u/99994203?v=4" alt="" />}
           <div className={styles.authorInfo}>
@@ -10,16 +10,20 @@ export function Post() {
             <span>FullStack Developer</span>
           </div>
         </div>
-        <time title='20 de Janeiro às 20:30' dateTime='2024-01-22 20:30'>Publicado há 1hr</time>
+        <time title='20 de Janeiro às 20:30' dateTime='2024-01-22 20:30'>Publicado há 1h</time>
+        </header>
 
         <div className={styles.content}>
-          
-           <p> Fala galeraaa 👋</p>
-           <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-           <p>👉<a href="">jane.design/doctorcare</a></p>
-           <p><a href="">#novoprojeto #nlw #rocketseat</a></p>          
+
+          <p> Fala galeraaa 👋</p>
+          <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+          <p>👉<a href=""> jane.design/doctorcare</a></p>
+          <p>
+            <a href="">#novoprojeto</a>
+            <a href=""> #nlw</a>
+            <a href=""> #rocketseat</a></p>
         </div>
-      </header>
+      
     </article>
   )
 
