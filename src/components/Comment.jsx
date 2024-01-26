a84cbc8593e4db2db64277568a6b2d633c8920b1
vitorbelluzzo <vitorbelluzzo@hotmail.com>
2024-01-25 21:02:13 -0300

    Estrutura da aplicação - Estilizações do Comentário

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -5,22 +5,26 @@ export function Comment() {
     <div className={styles.comment}>
       <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/99994203?v=4" alt="" />
       <div className={styles.commentBox}>
+        
         <div className={styles.commentContent}>
           <header>
             <div className={styles.authorAndTime}>
-              <strong>Vitor Belluzzo</strong>
-              <time title='20 de Janeiro às 20:30' dateTime='2024-01-22 20:30'>Cerca de 2h atrás</time>
+                <strong>Vitor Belluzzo</strong>
+                <time>Cerca de 2h atrás</time>
             </div>
 
             <button title='Deletar comentário'>
               <Trash size={20}/>
             </button>
           </header>
-          <p>Muito bom João, parabéns!!</p>
+            <p>Muito bom João, parabéns!!</p>
         </div>
+        
         <footer>
+          <button>
           <ThumbsUp size={20}/>
           Aplaudir <span>20</span>
+          </button>
         </footer>
 
       </div>
