5211359a319b9632c3f7a468b7f1e57fdfc559cf
vitorbelluzzo <vitorbelluzzo@hotmail.com>
2024-01-30 23:42:23 -0300

    Os motores do React - Propriedades do Post

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ import styles from "./app.module.css";
 import "./global.css";
 
 const posts = [
+  
   {
     id: 1,
     author: {
-      avatarUrl: "https://avatars.githubusercontent.com/u/99994203?v=4",
-      name: "Vitor Belluzzo",
-      role: "FullStack Developer",
+      avatarUrl: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpeloteros.com.ec%2Fsitioweb%2Fwp-content%2Fuploads%2F2022%2F01%2FIMG_20220104_163441.jpg&f=1&nofb=1&ipt=4cf02b7bfdfea521263d13062308b858c54413e134ce14c4247fb3992b04193c&ipo=images",
+      name: "Robert Arboleda",
+      role: "Zagueiro @SãoPauloFC",
     },
     content: [
       { type: "paragraph", content: "Fala galeraaa 👋" },
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,40 +1,54 @@
-import { Avatar } from './Avatar';
-import styles from './Post.module.css';
-import { Comment } from './comment';
+import { format, formatDistanceToNow } from "date-fns";
+import { ptBR } from "date-fns/locale/pt-BR";
+import { Avatar } from "./Avatar";
+import styles from "./Post.module.css";
+import { Comment } from "./comment";
 
+export function Post({ author, content, publishedAt }) {
+  const publishedDateFormatted = format(
+    publishedAt,
+    "d 'de' LLLL 'às' HH:mm'h'",
+    { locale: ptBR }
+  );
 
-export function Post(props) {
-  console.log(props);
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  });
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar  src="https://avatars.githubusercontent.com/u/99994203?v=4" alt="" />
+          <Avatar src={author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>Vitor Belluzzo</strong>
-            <span>FullStack Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
-        <time title='20 de Janeiro às 20:30' dateTime='2024-01-22 20:30'>Publicado há 1h</time>
+        <time
+          title={publishedDateFormatted}
+          dateTime={publishedAt.toISOString()}
+        >
+          {publishedDateRelativeToNow}
+        </time>
       </header>
 
       <div className={styles.content}>
+        {content.map((line) => {
+          if (line.type === "paragraph") {
+            return <p>{line.content}</p>;
+          } else if(line.type === "link"){
+            return <p><a href="#">{line.content}</a></p>;
 
-        <p> Fala galeraaa 👋</p>
-        <p>Estou animado por fazer parte desta comunidade inovadora. Vamos juntos explorar as possibilidades e construir conexões incríveis! 🚀</p>
-        <p>👉<a href=""> vitorbelluzzo/bubble</a></p>
-        <p>
-          <a href="">#novoprojeto</a>
-          <a href=""> #bubble</a>
-          <a href=""> #socialmedia</a></p>
+          }
+        })}
       </div>
-      <form className={styles.commentForm} >
+      <form className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
-        <textarea
-          placeholder="O que você achou do projeto?"
-        />
+        <textarea placeholder="O que você achou do projeto?" />
         <footer>
-          <button type='submit'>Comentar</button>
+          <button type="submit">Comentar</button>
         </footer>
       </form>
 
@@ -43,10 +57,6 @@ export function Post(props) {
         <Comment />
         <Comment />
       </div>
-
-
-
     </article>
-  )
-
+  );
 }
